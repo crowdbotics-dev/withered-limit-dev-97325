47b799d479905aaf2e42a0d619b092998f3827fe
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 12:48:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-limit-dev-97325",
+  url: "https://withered-limit-dev-97325.botics.co",
+  api: "https://withered-limit-dev-97325.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
